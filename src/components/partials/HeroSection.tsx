2834179c255a58d1730f39bc0fafd9a2bd6452e5
Zijie Cai <leo20020208@gmail.com>
2025-08-29 17:12:45 -0400

    Update HeroSection.tsx

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       <HeroBackground />
       <div className="flex flex-col items-center">
         <div className="overflow-hidden rounded-full">
-          <Image src="/images/avatar/man.png" width={180} height={180} alt="avatar" />
+          <Image src="/portfolio/images/avatar/man.png" width={180} height={180} alt="avatar" />
         </div>
         <h1 className="mt-4 text-3xl font-bold ">Mostafizur Rahman</h1>
         <div className="mt-2 flex gap-1">
